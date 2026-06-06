eb42739c77a813f765fc5e04ff83348cdc5bc19e
Irshad Ansari <er.irshadjainul@gmail.com>
2026-06-07 00:50:08 +0530

    updating cors links

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,15 @@ const cors=require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-code-reviewer-04.vercel.app",
+    "http://ai-code-reviewer-04.vercel.app",
+    ],
+  })
+);
 
 app.use(express.json())
 
