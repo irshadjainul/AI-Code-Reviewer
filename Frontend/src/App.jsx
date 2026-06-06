6a091f8b756edcfd30b927b6baab84a461df8e3b
Irshad Ansari <er.irshadjainul@gmail.com>
2026-06-07 01:27:22 +0530

    Update API endpoint for code review request

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }, []);
 
   async function reviewCode(){
-    const response=await axios.post('https://ai-code-reviewer-one-navy.vercel.app',{code})
+    const response=await axios.post('https://ai-code-reviewer-one-navy.vercel.app/api/get-review',{code})
     setReview(response.data)
 
   }
